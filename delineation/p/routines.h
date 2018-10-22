e3fa0095388894c0258d7ccc41e72e8fd217251e
Sergey Blintsov <sblin180@gmail.com>
2018-10-22 17:48:43 +0300

    routines in p released

--- delineation/p/routines.h
+++ delineation/p/routines.h
@@ -11,7 +11,7 @@
 
 int get_p_wdc_scale_id(const ECGLead& ecg_lead) {
 
-    int num_wdc_scales = ecg_lead.wdc.size();
+    unsigned long num_wdc_scales = ecg_lead.wdc.size();
 
     int wdc_scale_id = int(WDC_SCALE_ID);
 
@@ -30,14 +30,14 @@ int get_window(const ECGLead& ecg_lead, size_t qrs_id) {
 
     size_t qrs_gap = qrs_dels[qrs_id].onset_index - qrs_dels[qrs_id - 1].offset_index;
 
-    int window_candidate_1 = int(qrs_gap * float(ALPHA_ZCS_QRS_GAP));
-    int window_candidate_2 = int(rate * float(ALPHA_ZCS_WINDOW));
+    auto window_candidate_1 = int(qrs_gap * float(ALPHA_ZCS_QRS_GAP));
+    auto window_candidate_2 = int(rate * float(ALPHA_ZCS_WINDOW));
 
     int window = std::min(window_candidate_1, window_candidate_2);
 
     std::vector<WaveDelineation> t_dels = ecg_lead.t_dels;
 
-    if (t_dels) {
+    if (!t_dels.empty()) {
         unsigned long corr_t_id = std::min(qrs_id - 1, t_dels.size() - 1);
         size_t left_diff = qrs_dels[qrs_id].onset_index - t_dels[corr_t_id].offset_index;
 
@@ -47,48 +47,33 @@ int get_window(const ECGLead& ecg_lead, size_t qrs_id) {
         }
 
         if (left_diff > 0) {
-            int window_candidate_3 = left_diff;
+            auto window_candidate_3 = int(left_diff);
             window = std::min(window, window_candidate_3);
         }
     }
     return window;
 }
 
-        def get_p_begin_index(ecg_lead, qrs_id):
+size_t get_p_begin_index(const ECGLead& ecg_lead, size_t qrs_id) {
+    std::vector<WaveDelineation> qrs_dels = ecg_lead.qrs_dels;
 
-"""
-Define begin index for P delineation
-:param ecg_lead: certain ECG lead
-:param qrs_id: id of QRS, to the left of which we delineate P
-:return begin_index: begin index for P delineation
-"""
+    int window = get_window(ecg_lead, qrs_id);
+    size_t begin_index = qrs_dels[qrs_id].onset_index - window;
 
-qrs_dels = ecg_lead.qrs_dels
+    return begin_index;
+}
 
-window = get_window(ecg_lead, qrs_id)
-begin_index = qrs_dels[qrs_id].onset_index - window
+size_t get_p_end_index(const ECGLead& ecg_lead, size_t qrs_id) {
+    std::vector<WaveDelineation> qrs_dels = ecg_lead.qrs_dels;
+    int wdc_scale_id = get_p_wdc_scale_id(ecg_lead);
+    std::vector<double> wdc = ecg_lead.wdc[wdc_scale_id];
 
-return begin_index
+    int window = get_window(ecg_lead, qrs_id);
+    size_t begin_index = qrs_dels[qrs_id].onset_index - window;
+    ModulusMaxima tmp_mm = find_left_mm(qrs_dels[qrs_id].onset_index, wdc);
+    size_t end_index_candidate_1 = tmp_mm.index;
+    size_t end_index_candidate_2 = find_left_thc_index(wdc, qrs_dels[qrs_id].onset_index, int(begin_index), 0.0);
+    size_t end_index = std::max(end_index_candidate_1, end_index_candidate_2 - 1);
 
-
-        def get_p_end_index(ecg_lead, qrs_id):
-
-"""
-Define end index for P delineation
-:param ecg_lead: certain ECG lead
-:param qrs_id: id of QRS, to the left of which we delineate P
-:return end_index: end index for P delineation
-"""
-
-qrs_dels = ecg_lead.qrs_dels
-wdc_scale_id = get_p_wdc_scale_id(ecg_lead)
-wdc = ecg_lead.wdc[wdc_scale_id]
-
-window = get_window(ecg_lead, qrs_id)
-begin_index = qrs_dels[qrs_id].onset_index - window
-tmp_mm = find_left_mm(qrs_dels[qrs_id].onset_index, wdc)
-end_index_candidate_1 = tmp_mm.index
-end_index_candidate_2 = find_left_thc_index(wdc, qrs_dels[qrs_id].onset_index, begin_index, 0.0)
-end_index = max(end_index_candidate_1, end_index_candidate_2 - 1)
-
-return end_index
+    return end_index;
+}
