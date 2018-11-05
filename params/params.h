66f1a2601084796a1f0962e396df1f352fc04bf0
Sergey Blintsov <sblin180@gmail.com>
2018-11-06 00:18:08 +0300

    params/params new params added

--- params/params.h
+++ params/params.h
@@ -1,6 +1,6 @@
 #pragma once
 
-static const size_t WDC_SCALE_ID = 3;
+static const int WDC_SCALE_ID = 3;
 static const double GAMMA_SCALES_DIFF = 3.0;
 static const double GAMMA_LEFT_Q_PART = 0.7;
 static const double GAMMA_LEFT_ODD_XTD_ZCS_SHIFT = 0.03;
@@ -44,7 +44,7 @@ static const double GAMMA_RIGHT_XTD_ZCS_MM_PART = 0.12;
 
 
 static const double GAMMA_LEFT_WINDOW = 0.03;
-static const size_t GAMMA_BORD_SCALE = 2;
+static const int GAMMA_BORD_SCALE = 2;
 
 
 static const double GAMMA_RIGHT_WINDOW = 0.03;
@@ -53,3 +53,64 @@ static const double GAMMA_RIGHT_WINDOW = 0.03;
 static const double EXTRA_BEAT_PART = 0.7;
 static const double ALPHA_HUGE_PART = 0.8;
 static const double ALPHA_INC = 0.1;
+
+
+static const double ALPHA_MM_WINDOW = 0.1;
+static const double ALPHA_ZCS_QRS_GAP = 0.47;
+static const double ALPHA_ZCS_WINDOW = 0.37;
+static const double ALPHA_RIGHT_MM_DIST = 0.08;
+static const double ALPHA_OFFSET_MM_SHARP = 6.00;
+static const double ALPHA_LEFT_MM_DIST = 0.08;
+static const double ALPHA_ONSET_MM_SHARP = 6.00;
+static const double ALPHA_LEFT_ZC_PART_SHIFT = 1.0;
+static const double ALPHA_RIGHT_ZC_PART_SHIFT = 1.0;
+static const double ALPHA_PEAK_SMALL_AMPL = 0.020;
+static const double ALPHA_PEAK_BEGIN_SHIFT = 0.05;
+static const double ALPHA_PEAK_ADAPT_BEGIN_PART = 0.5;
+static const double ALPHA_PEAK_ADAPT_POW = 2.0;
+static const double ALPHA_PEAK_ADAPT_AMPL = 0.1;
+static const double ALPHA_FLEX_SHIFT = 0.030;
+static const double ALPHA_FLEX_AMPL_NGBR = 0.45;
+static const double ALPHA_FLEX_AMPL_OLD_ZC = 0.35;
+static const double ALPHA_BIPHASE_AMPL_MORE = 0.45;
+static const double ALPHA_BIPHASE_AMPL_LESS = 2.2;
+static const double ALPHA_BIPHASE_AMPL = 0.75;
+static const double ALPHA_ONSET_MM = 0.4;
+static const double ALPHA_OFFSET_MM = 0.4;
+static const double ALPHA_OFFSET_MM_OVERFLOW = 4.0;
+static const double ALPHA_ONSET_OFFSET_THR = 0.25;
+
+static const int LEGACY_FIB_NUM_ZCS = 4;
+static const double LEGACY_FIB_STD = 0.15;
+
+static const double FIB_LEN_PART = 4.0;
+static const double FIB_AMPL_PART = 4.0;
+static const double FIB_DELS_PART = 0.6;
+
+static const int BETA_SCALE = 3;
+static const double BETA_PEAK_ZC_AMPL = 0.7;
+
+// NEW
+static const double MM_WINDOW = 0.1;
+static const double RIGHT_MM_DIST = 0.08;
+static const double OFFSET_MM_SHARPNESS = 3.0;
+static const double LEFT_MM_DIST = 0.08;
+static const double ONSET_MM_SHARPNESS = 5.0;
+static const double ZCS_PEAK_SEARCHING_SHIFT = 0.08;
+static const int FIB_NUM_ZCS = 2;
+static const double FIB_STD = 0.15;
+static const double LOW_LIMIT_AMPLITUDE = 0.03;
+static const double OFFSET_MM_COEFF = 0.4;
+static const double OFFSET_MM_COEFF_OVERFLOW = 4.0;
+static const double ONSET_OFFSET_THRESHOLD = 0.25;
+static const double ONSET_MM_COEFF = 0.4;
+static const double PEAK_ZC_AMPLITUDE_DECREASING_LEFT_PRIVILEGE = 0.5;
+static const double PEAK_ZC_AMPLITUDE_DECREASING_RIGHT_PRIVILEGE = 0.2;
+static const double PEAK_ZC_AMPLITUDE_DECREASING_BEGIN_PART = 0.4;
+static const double PEAK_ZC_AMPLITUDE_DECREASING_POW = 2.0;
+static const double PEAK_ZC_AMPLITUDE_DECREASING = 0.5;
+static const double FLEXURE_SHIFT = 0.03;
+static const double FLEXURE_AMPLITUDE_NEIGHBOUR = 0.45;
+static const double FLEXURE_AMPLITUDE_OLD_ZC = 0.35;
+static const double BEGIN_QRS_GAP_PROPORTION = 0.45;
+static const double ZCS_SEARCHING_WINDOW = 0.3;
